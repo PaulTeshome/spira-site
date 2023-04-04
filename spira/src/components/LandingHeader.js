47bd67541e58fc5e7dc9b5bb637d1dda6143dba3
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-04 17:39:39 +0300

    header update
    first draft header

--- spira/src/components/LandingHeader.js
+++ spira/src/components/LandingHeader.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import './LandingHeader.css'
+import logo from '../images/spira_logo1 1.png'
 
 function LandingHeader() {
   return (
+    <div>
     <div className='header-container'>
-        Services
-        About us
-        <button className="hireBtn">Hire us</button>
+       <button className='menuItem hireBtn'>Hire us</button>
+        <a className='menuItem' href='app.js'>Services</a>
+        <span className='menuItem'>About us</span>
+        <a  href='app.js'>
+          <img className='menuLogo' src={logo} alt="spira logo"/>
+        </a>
     </div>
+    <div className='quote-container'>
+        <span className="quote">EMPOWREING YOUR BRAND IN THE DIGITAL SPACE</span>
+ </div>
+ </div>
   )
 }
 

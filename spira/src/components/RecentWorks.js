7b464cba34819e4678eee49d9bb1ec5732ad5192
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-08 00:09:06 +0300

    Update RecentWorks.js

--- spira/src/components/RecentWorks.js
+++ spira/src/components/RecentWorks.js
@@ -7,9 +7,9 @@ function RecentWorks() {
     <div className='recent-container'>
        <span>RECENT WORKS</span>
         <div className='work-list-container'>
-            <RecentWorksCard/>
-            <RecentWorksCard/>
-            <RecentWorksCard/>
+            <RecentWorksCard proj_title="Project 1" proj_desc=" this is the proj description"/>
+            <RecentWorksCard proj_title="Project 2" proj_desc=" this is the proj description"/>
+            <RecentWorksCard proj_title="Project 3" proj_desc=" this is the proj description"/>
         </div>
     </div>
   )

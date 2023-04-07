7afd6d6d1ffcf13e190cbbfd1d0697f1959af59d
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-07 20:45:04 +0300

    Update RecentWorksCard.js

--- spira/src/components/RecentWorksCard.js
+++ spira/src/components/RecentWorksCard.js
@@ -4,7 +4,10 @@ import './RecentWorksCard.css'
 function RecentWorksCard() {
   return (
     <div className='recent-card'>
-        
+      <div className='recent-card-bg'></div>
+        <div className='recent-card-desc'>
+          awbdhjaldbwajldwaljdhwjalfjek;kl ai amapaulsoenfe
+        </div>
     </div>
   )
 }

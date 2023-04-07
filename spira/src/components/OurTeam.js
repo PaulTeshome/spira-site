4ae19a9aefe97ed91856d62665034399abf293c6
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-08 00:09:25 +0300

    Create OurTeam.js

---
+++ spira/src/components/OurTeam.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './OurTeam.css'
+import MemberCard from './MemberCard'
+import profile from '../images/default_profile.png'
+import profile2 from '../images/stockholm2.jpg'
+
+
+function OurTeam() {
+  return (
+    <div className='team-container'>
+        <span className='team-header'>OUR TEAM</span>
+        <p className='team-desc'>
+             Our team of experienced professionals is passionate about creating engaging content and delivering effective digital marketing strategies that drive business growth.
+            We use the latest technology and techniques to ensure that your brand stays relevant and competitive in today's fast-paced digital landscape.
+            We are committed to providing our clients with exceptional service and delivering measurable results. 
+            Let us help you take your brand to the next level.
+            <br/><a href="#contact">Contact us</a>  today to learn more about how we can help your business thrive.
+        </p>
+        <div className='team-cards-container'>
+            <MemberCard name="Sidiki Daiwara" title="Creative Director" source={profile}/>
+            <MemberCard name="Evelina Goussi" title="Graphic Designer" source={profile}/>
+            <MemberCard name="Kevin Jabro" title="Marketor" source={profile2}/>
+        </div>
+    </div>
+  )
+}
+
+export default OurTeam

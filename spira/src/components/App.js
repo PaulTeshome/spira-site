7fe1f657cd954852cb8c433635a19b06eb221a74
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-07 14:37:36 +0300

    Update App.js

--- spira/src/components/App.js
+++ spira/src/components/App.js
@@ -1,11 +1,21 @@
 // import logo from '../images/spira_logo1 1.png';
 import './App.css';
 import LandingHeader from './LandingHeader';
+import MottoSection from './MottoSection';
+import Services from './Services';
+import RecentWorks from './RecentWorks';
 
 function App() {
   return (
     <div className="App">
       <LandingHeader/>
+      <MottoSection motto="EMPOWERING YOUR BRAND IN THE DIGITAL SPACE" description="
+                At Spira, our mission is to provide personalized and results-driven solutions
+                to help our clients achieve their business goals. 
+                We believe in building strong relationships with our clients 
+                and working closely with them to understand their unique needs and challenges."/>
+      <Services/>
+      <RecentWorks/>
     </div>
   );
 }

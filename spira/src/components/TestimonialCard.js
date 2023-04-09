0d5668420ed09363a9ab8cfdc30fda96473338c0
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-09 23:55:54 +0300

    Update TestimonialCard.js
    testimonial card body update

--- spira/src/components/TestimonialCard.js
+++ spira/src/components/TestimonialCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function TestimonialCard() {
+function TestimonialCard({source,testimonial,owner}) {
   return (
     <div className='testimonial-card'>
-
+        <div className='testimonial-profile-holder'>
+            <img className='testimonial-profile' src={source} alt='testimonial profile'/>
+        </div>
+        <div className='testimonial-text-holder'>
+            <span className='testimonial-text'>"{testimonial}"</span>
+            <span className='testimonial-owner'>- {owner}</span>
+        </div>
     </div>
   )
 }

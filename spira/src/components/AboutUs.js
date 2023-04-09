a8e328e4c709c7b023e80c80528fef70c802882b
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-09 23:57:04 +0300

    update testimonial section
    updating testimonial section in about us

--- spira/src/components/AboutUs.js
+++ spira/src/components/AboutUs.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import TestimonialCard from './TestimonialCard'
 import './AboutUs.css'
 import illust4 from '../images/about us illustration.svg'
+import profile from '../images/default_profile.png'
 
 function AboutUs() {
   return (
-    <div className='aboutUs-container'>
+    <div className='aboutUs-container' id="abtUs">
               <span className='aboutUs-title'>ABOUT US</span>
 
               <div className='aboutUs-desc'>
-                  <div className="aboutUs-description" id="abtUs">
+                  <div className="aboutUs-description">
                       <span >Welcome to Spira, your go-to digital marketing agency for all your social
                         media and accounting management needs. We specialize in content creation, 
                         marketing, and graphic design to help your brand stand out in the digital 
@@ -23,8 +25,14 @@ function AboutUs() {
                   </div>
               </div>
 
-              <div className='testimonials'>
-
+              <div className='aboutUs-testimonials'>
+                <div className='testimonials-scroller'>
+                  <TestimonialCard source={profile} testimonial="Best marketing agency to work with. Amazing services" owner="unknown1"/>
+                  <TestimonialCard source={profile} testimonial="Best marketing agency to work with. Amazing services" owner="unknown2"/>
+                  <TestimonialCard source={profile} testimonial="Best marketing agency to work with. Amazing services" owner="unknown3"/>
+                  <TestimonialCard source={profile} testimonial="Best marketing agency to work with. Amazing services" owner="unknown4"/>
+                </div>
+                <button className='last-hire-btn'>Hire Us</button>
               </div>
              
            </div>

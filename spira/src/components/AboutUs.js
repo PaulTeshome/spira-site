f0919bbef0398d0df984560243bc84a7e5feb926
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-09 16:18:36 +0300

    Update AboutUs.js
    update on structure

--- spira/src/components/AboutUs.js
+++ spira/src/components/AboutUs.js
@@ -4,23 +4,29 @@ import illust4 from '../images/about us illustration.svg'
 
 function AboutUs() {
   return (
-    <div className='aboutUs-wrapper'>
-            <div className='aboutUs-illust'>
-                  <img id='illust4' src={illust4} alt='about us illustration'/>
+    <div className='aboutUs-container'>
+              <span className='aboutUs-title'>ABOUT US</span>
+
+              <div className='aboutUs-desc'>
+                  <div className="aboutUs-description" id="abtUs">
+                      <span >Welcome to Spira, your go-to digital marketing agency for all your social
+                        media and accounting management needs. We specialize in content creation, 
+                        marketing, and graphic design to help your brand stand out in the digital 
+                        world.<br/><br/>
+                        At Spira, our mission is to provide personalized and results-driven 
+                        solutions to help our clients achieve their business goals. We believe in
+                        building strong relationships with our clients and working closely with 
+                        them to understand their unique needs and challenges.</span>
+                  </div>
+                  <div className='aboutUs-illust'>
+                      <img id='illust4' src={illust4} alt='about us illustration'/>
+                  </div>
               </div>
-              <div className='title'>
-              <span className='title'>ABOUT US</span>
+
+              <div className='testimonials'>
+
               </div>
-              <div className="aboutUs-description">
-                  <span >Welcome to Spira, your go-to digital marketing agency for all your social
-                     media and accounting management needs. We specialize in content creation, 
-                     marketing, and graphic design to help your brand stand out in the digital 
-                     world.<br/><br/>
-                     At Spira, our mission is to provide personalized and results-driven 
-                     solutions to help our clients achieve their business goals. We believe in
-                     building strong relationships with our clients and working closely with 
-                     them to understand their unique needs and challenges.</span>
-              </div>
+             
            </div>
   )
 }

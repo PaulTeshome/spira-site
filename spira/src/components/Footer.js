01d9fc55d26705e178e9af1563d4c9787deee62b
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-10 12:28:12 +0300

    Update Footer.js

--- spira/src/components/Footer.js
+++ spira/src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
+  let date = new Date();
   return (
     <footer className='spira-footer'>
+      <span className='footer-title'>SPIRA</span>
+      <div className='footer-content'>
+        <div className='footer-contacts'>
 
+        </div>
+        <div className='footer-links'>
+
+
+        </div>
+      </div>
+      <span className='footer-end'>
+        Copyright &copy;  {date.getFullYear()} Digital Marketing Agency. All rights reserved.
+      </span>
     </footer>
   )
 }

eebf29070e2fcc189d32c2d4316b419ff2d28a6a
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-10 14:36:25 +0300

    Update Footer.js
    update on footer content

--- spira/src/components/Footer.js
+++ spira/src/components/Footer.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import './Footer.css'
+import tg_logo from '../images/tg logo.png'
+import insta_logo from '../images/insta logo.png'
+import mail_logo from '../images/mail logo.png'
 
-function Footer() {
+function Footer({telegram_link,insta_link,gmail_link,phone1,phone2,email,location}) {
   let date = new Date();
   return (
     <footer className='spira-footer'>
       <span className='footer-title'>SPIRA</span>
       <div className='footer-content'>
         <div className='footer-contacts'>
+          <span className='footer-contacts-header'>Contact Us</span>
+          <span>Email: <a href={email}>{email}</a></span>
+          <span>Phone: {phone1},{phone2}</span>
+          <span>Location: {location}</span>
 
         </div>
         <div className='footer-links'>
-
-
+            <a href={telegram_link}><img src={tg_logo} alt="telegram link"/></a>
+            <a href={gmail_link}><img src={mail_logo} alt="telegram link"/></a>
+            <a href={insta_link}><img src={insta_logo} alt="telegram link"/></a>
         </div>
       </div>
       <span className='footer-end'>

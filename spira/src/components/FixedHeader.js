bd76cbf8b7cae7db08779d21a3ab1df85ae7f506
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-18 19:26:32 +0300

    Update FixedHeader.js
    customisabe header adjustments

--- spira/src/components/FixedHeader.js
+++ spira/src/components/FixedHeader.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import logo from '../images/spira_logo1 1.png'
 
-function FixedHeader({services, about}) {
+function FixedHeader({logo_link,services, about}) {
   return (
     <div className='fixed-header col-12'>
         <button className='menu-item hire-btn col-1'>Hire us</button>
         <a className='menu-item' href={services}>Services</a>
         <a className='menu-item' href={about}>About Us</a>
-        <a  href='app.js'>
+        <a  href={logo_link}>
             <img className='menu-logo col-0' src={logo} alt="spira logo"/>
         </a>
     </div>

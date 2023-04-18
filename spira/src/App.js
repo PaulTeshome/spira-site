39f693a598d58832bde0d57ae1cfecf128454dac
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-18 19:26:56 +0300

    Update App location

---
+++ spira/src/App.js
@@ -0,0 +1,12 @@
+// import logo from '../images/spira_logo1 1.png';
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;

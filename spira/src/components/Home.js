bbf70f1aa1b96b324879f6ad2161d391d8a96092
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-18 19:25:25 +0300

    Create Home
    create home page for easy routing

---
+++ spira/src/components/Home.js
@@ -0,0 +1,31 @@
+// import logo from '../images/spira_logo1 1.png';
+import './Home.css';
+import LandingHeader from './LandingHeader';
+import MottoSection from './MottoSection';
+import Services from './Services';
+import RecentWorks from './RecentWorks';
+import OurTeam from './OurTeam';
+import AboutUs from './AboutUs';
+import Footer from './Footer';
+
+
+function Home() {
+  return (
+    <div className="App">
+      <LandingHeader/>
+      <MottoSection motto="EMPOWERING YOUR BRAND IN THE DIGITAL SPACE" description="
+                At Spira, our mission is to provide personalized and result-driven solutions
+                to help our clients achieve their business goals. 
+                We believe in building strong relationships with our clients 
+                and working closely with them to understand their unique needs and challenges."/>
+      <Services/>
+      <RecentWorks/>
+      <OurTeam/>
+      <AboutUs/>
+      <Footer telegram_link="#tg" insta_link="#insta" gmail_link="email" phone1="+46731443749" 
+              phone2="+46762727223" email="info@spiraagency.com" location="Stockholm, Sweden"/>
+    </div>
+  );
+}
+
+export default Home;

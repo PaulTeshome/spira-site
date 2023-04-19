116c88cee1135243e25f2f815b42e310df277ec5
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-20 00:14:04 +0300

    Update FixedHeader.js

--- spira/src/components/FixedHeader.js
+++ spira/src/components/FixedHeader.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import logo from '../images/spira_logo1 1.png'
+import { HashLink as Link } from 'react-router-hash-link'
 
-function FixedHeader({logo_link,services, about}) {
+function FixedHeader({logo_link,services, about, hireUs}) {
+  const fixedHeaderBg= hireUs===""?'fixed-header-hire':'fixed-header'
   return (
-    <div className='fixed-header col-12'>
-        <button className='menu-item hire-btn col-1'>Hire us</button>
-        <a className='menu-item' href={services}>Services</a>
-        <a className='menu-item' href={about}>About Us</a>
-        <a  href={logo_link}>
+    <nav className={`${fixedHeaderBg} col-12`}>
+        <Link className={hireUs===""?'invisible-hire':'menu-item hire-btn col-1'} to={hireUs} smooth>Hire us</Link>
+        <Link className='menu-item' to={services} smooth>Services</Link>
+        <Link className='menu-item' to={about} smooth>About Us</Link>
+        <Link  to={logo_link} smooth>
             <img className='menu-logo col-0' src={logo} alt="spira logo"/>
-        </a>
-    </div>
+        </Link>
+    </nav>
   )
 }
 

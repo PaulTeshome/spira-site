9c4d5ce6422e4536aeace526b57c3982df59af77
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-20 00:15:02 +0300

    Update HireForm.js

--- spira/src/components/HireForm.js
+++ spira/src/components/HireForm.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import './HireForm.css'
 import InputComponent from './InputComponent'
+import FixedHeader from './FixedHeader'
+import RadioComponent from './RadioComponent'
 
 function HireForm() {
   return (
     <div className='hire-form-container'>
-      <div className='form-container'>
-        {/* <div className='contract-pic'> 
-        </div> */}
+      <FixedHeader logo_link="/" services="/#services" about="/#abtUs" hireUs=""/>
+      <div className='form-container' id="top">
         <form className='hire-form'>
-            <InputComponent label="First Name" type="text" name="fname" placeholder='Enter First Name'/>
-            <InputComponent label="Last Name" type="text" name="lname" placeholder='Enter Last Name'/>
-            <InputComponent  label="Email" type="Email" name="email" placeholder='Enter your email'/>
-            <InputComponent label="Company Name" type="text" name="cname" placeholder='Enter Company Name'/>
-            <InputComponent label="Phone" type="tel" name="phone" placeholder='Enter Phone Number'/>
+            <InputComponent label="First Name" type="text" id="fname" name="fname" placeholder='Enter First Name'/>
+            <InputComponent label="Last Name" type="text" id="lname" name="lname" placeholder='Enter Last Name'/>
+            <InputComponent  label="Email" type="Email" id="email" name="email" placeholder='Enter your email'/>
+            <InputComponent label="Company Name" type="text" id="cname" name="cname" placeholder='Enter Company Name'/>
+            <InputComponent label="Phone" type="tel" id="phone" name="phone" placeholder='Enter Phone Number'/>
             <label>
-            Tell us how we could be of help<br/>
-
-            <input type="radio" value="Marketing" name="marketing" checked="true" /> Marketing<br/>
-            <input type="radio" value="Accounting Management" name="accounting" /> Accounting Management<br/>
-            <input type="radio" value="contentCreation" name="content" /> Content Creation<br/>
-            <input type="radio" value="other" name="content" /> Other<br/>
-            </label>
+               Tell us how we could be of help
+            </label><br/>
+            
+            <RadioComponent value="marketing" label="Marketing" checked={true}/>
+            <RadioComponent value="accMgmt" label="Accounting Management" checked={true}/>
+            <RadioComponent value="content" label="Content Creation" checked={true}/>
+            <RadioComponent value="other" label="Other" checked={true}/>
+            
             <label>
                 Specify<br/>
             <textarea name="spec" placeholder='Specify your choice above'/>
             </label>
+            
             <InputComponent type="submit" name="submit" value="Submit"/>
         </form>
       </div>

2d7f632f3be6866756442414628fb07a9e30066f
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-22 12:23:20 +0300

    Create AdminNav.js

---
+++ spira/src/components/AdminNav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './AdminNav.css'
+import AdminNavComp from './AdminNavComp'
+function AdminNav() {
+  return (
+    <nav className='admin-nav'>
+        <AdminNavComp link="*/*" name="Dashboard"/>
+        <AdminNavComp link="*/pagecustom" name="Page Customization"/>
+        <AdminNavComp link="*" name="Manage Adminstrators"/>
+        <AdminNavComp link="../" name="Logout"/>
+    </nav>
+  )
+}
+
+export default AdminNav

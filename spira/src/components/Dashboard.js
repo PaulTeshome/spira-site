b236f5a39d6f3bef8b0a8de14b96fd682ee820f7
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-22 21:08:06 +0300

    Update Dashboard.js

--- spira/src/components/Dashboard.js
+++ spira/src/components/Dashboard.js
@@ -5,22 +5,25 @@ import TableRow from './TableRow'
 function Dashboard() {
   return (
     <div className='dashboard-holder' id='dashtop'>
-        <h2 className='dashboard-title'>Dashboard</h2>
-        <p>This is the list of customers that have sent a request for hire ordered from latest to earliest</p>
+        <span className='dashboard-title'>Dashboard</span>
+        <span className='dashboard-intro'>This is the list of customers that have sent a request for hire, ordered from latest to earliest.</span>
         <div className='table-holder'>
         <table className='request-table'>
-            <tr>
-                <th>Request ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Company Name</th>
-                <th>Phone</th>
-                <th>Service</th>
-                <th>Service Spec</th>
-                <th>Status</th>
-                <th>Button</th>
-            </tr>
+           
+            <thead>
+                <tr>
+                    <th>Request ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Company Name</th>
+                    <th>Phone</th>
+                    <th>Service</th>
+                    <th>Service Spec</th>
+                    <th>Status</th>
+                    <th>Button</th>
+                </tr>                
+            </thead>
             <TableRow ReqId="1" fname="paul" lname="teshe" email="aaa@gmail.com" compName="pied piper" phone="107232801" service="marketing management" specify="null" status={true}/>
             <TableRow ReqId="1" fname="paul" lname="teshe" email="aaa@gmail.com" compName="pied piper" phone="107232801" service="marketing management" specify="null" status={false}/>
             <TableRow ReqId="1" fname="paul" lname="teshe" email="aaa@gmail.com" compName="pied piper" phone="107232801" service="marketing management" specify="null" status={true}/>

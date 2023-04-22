dc11a3cd2961731c2046a635d6e0400b70876ac1
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-22 12:23:10 +0300

    Update OurTeam.js

--- spira/src/components/OurTeam.js
+++ spira/src/components/OurTeam.js
@@ -3,6 +3,7 @@ import './OurTeam.css'
 import MemberCard from './MemberCard'
 import profile from '../images/default_profile.png'
 import profile2 from '../images/stockholm2.jpg'
+import { HashLink as Link } from 'react-router-hash-link'
 
 
 function OurTeam() {
@@ -14,7 +15,7 @@ function OurTeam() {
             We use the latest technology and techniques to ensure that your brand stays relevant and competitive in today's fast-paced digital landscape.
             We are committed to providing our clients with exceptional service and delivering measurable results. 
             Let us help you take your brand to the next level.
-            <br/><a href="#contact">Contact us</a>  today to learn more about how we can help your business thrive.
+            <br/><Link to="/#contact" smooth>Contact us</Link>  today to learn more about how we can help your business thrive.
         </p>
         <div className='team-cards-container'>
             <MemberCard name="Sidiki Daiwara" title="Creative Director" source={profile}/>

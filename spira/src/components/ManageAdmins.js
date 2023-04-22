11090329a49c22c495cd853b7a39ac6369d2df03
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-23 01:53:01 +0300

    Create ManageAdmins.js

---
+++ spira/src/components/ManageAdmins.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ManageAdmins.css'
+import SettingCard from './SettingCard'
+
+function ManageAdmins() {
+  return (
+    <div className='manage-admin-holder'>
+      <span className='manage-title'>Admin Managment</span>
+      <span className='manage-intro'>Welcome to admin manage! Here you can edit, add or remove admins for your system.
+                                             Please select from the options below to proceed accordingly.</span>
+        <SettingCard title="Edit own credentials" desc="Edit current logged in admin's account detail" link="/"/>
+        <SettingCard title="Add/Remove admin" desc="Add or remove admin to/from the system" link="/"/>
+    </div>
+  )
+}
+
+export default ManageAdmins

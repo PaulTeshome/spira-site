7db49497753c50722ef2cc97069972fc837ed64d
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-22 21:07:21 +0300

    Update InputComponent.js

--- spira/src/components/InputComponent.js
+++ spira/src/components/InputComponent.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './InputComponent.css'
 
-function InputComponent({label, type, name, id, placeholder, value, onchange}) {
+function InputComponent({label, type, name, id, placeholder, value, onchange,classNm}) {
   return (
     <div className='input-holder'>
         <label className='label-text'>
         {label}
         </label>
-        <input type={type} name={name} id={id} placeholder={placeholder} className='form-inputs' value={value} onChange={onchange}/><br/>
+        <input type={type} name={name} id={id} placeholder={placeholder} className={classNm} value={value} onChange={onchange}/><br/>
     </div>
   )
 }

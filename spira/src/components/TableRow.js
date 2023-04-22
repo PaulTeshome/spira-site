1be1e7a68777f1e164998360d53d54dba8e32b1f
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-22 21:07:12 +0300

    Update TableRow.js

--- spira/src/components/TableRow.js
+++ spira/src/components/TableRow.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import InputComponent from './InputComponent'
 
 function TableRow({ReqId,fname,lname,email,compName,phone,service,specify,status}) {
     const stat= status?'new':'seen'
+    const placeholder= status?'mark as seen':'mark as new'
+    const classNm =status?'table-btn2':'table-btn'
   return (
-    <tr>
-        <td>{ReqId}</td>
-        <td>{fname}</td>
-        <td>{lname}</td>
-        <td>{email}</td>
-        <td>{compName}</td>
-        <td>{phone}</td>
-        <td>{service}</td>
-        <td>{specify}</td>
-        <td className={stat}>{stat}</td>
-        <td>Button</td>
-    </tr>
+    <tbody>
+        <tr>
+            <td>{ReqId}</td>
+            <td>{fname}</td>
+            <td>{lname}</td>
+            <td>{email}</td>
+            <td>{compName}</td>
+            <td>{phone}</td>
+            <td>{service}</td>
+            <td>{specify}</td>
+            <td className={stat}>{stat}</td>
+            <td>
+                <InputComponent type="submit"  value={placeholder} classNm={classNm}/>
+            </td>
+        </tr>
+    </tbody>
+   
   )
 }
 

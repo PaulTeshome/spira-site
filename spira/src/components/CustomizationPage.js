77dd3cb89b226c296a12fd420942df46a28ef9b6
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-22 12:23:12 +0300

    Create CustomizationPage.js

---
+++ spira/src/components/CustomizationPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CustomizationPage.css'
+
+function CustomizationPage() {
+  return (
+    <div className='custom-page-holder'>
+        CustomizationPage
+        
+    </div>
+  )
+}
+
+export default CustomizationPage

22d7a79c8c684679553f1a4eb80880cfae7d539b
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-22 12:21:59 +0300

    Update HireForm.js

--- spira/src/components/HireForm.js
+++ spira/src/components/HireForm.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './HireForm.css'
 import InputComponent from './InputComponent'
+import TextInput from './TextInput'
 import FixedHeader from './FixedHeader'
 import RadioComponent from './RadioComponent'
 
 function HireForm() {
   return (
     <div className='hire-form-container'>
-      <FixedHeader logo_link="/" services="/#services" about="/#abtUs" hireUs=""/>
+      <FixedHeader logo_link="/#home" services="/#services" about="/#abtUs" hireUs=""/>
       <div className='form-container' id="top">
         <form className='hire-form'>
             <InputComponent label="First Name" type="text" id="fname" name="fname" placeholder='Enter First Name'/>
@@ -15,6 +16,7 @@ function HireForm() {
             <InputComponent  label="Email" type="Email" id="email" name="email" placeholder='Enter your email'/>
             <InputComponent label="Company Name" type="text" id="cname" name="cname" placeholder='Enter Company Name'/>
             <InputComponent label="Phone" type="tel" id="phone" name="phone" placeholder='Enter Phone Number'/>
+            
             <label>
                Tell us how we could be of help
             </label><br/>
@@ -24,11 +26,8 @@ function HireForm() {
             <RadioComponent value="content" label="Content Creation" checked={true}/>
             <RadioComponent value="other" label="Other" checked={true}/>
             
-            <label>
-                Specify<br/>
-            <textarea name="spec" placeholder='Specify your choice above'/>
-            </label>
-            
+            <TextInput textLabel="Specify" name="spec"  placeholder='Specify your choice above'/>
+
             <InputComponent type="submit" name="submit" value="Submit"/>
         </form>
       </div>

4e2991aa3a03c4a95e0b61086dacb4b298ef4576
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-23 10:30:28 +0300

    Update RadioComponent.js

--- spira/src/components/RadioComponent.js
+++ spira/src/components/RadioComponent.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './RadioComponent.css'
 
-function RadioComponent({value,label}) {
+function RadioComponent({value,label,name,onchange,checked}) {
   return (
     <div className="radio">
         <label>
         <input
             type="radio"
             value={value}
+            name={name}
+            onChange={onchange}
+            checked={checked}
             // checked={this.state.selectedOption === {"Other"}}
             // onChange={this.onValueChange}
         />

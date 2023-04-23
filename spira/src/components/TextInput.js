a9511159fab207d837441aa05e11642cad0dd43b
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-23 10:30:19 +0300

    Update TextInput.js
    insert onchange

--- spira/src/components/TextInput.js
+++ spira/src/components/TextInput.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './TextInput.css'
 
-function TextInput({textLabel,name,value,placeholder}) {
+function TextInput({textLabel,name,value,placeholder,onchange}) {
   return (
     <div className='text-input-holder'>
         <label className='text-input-label'>
             {textLabel}<br/>
-            <textarea className='text-inputs' name={name} placeholder={placeholder} value={value}/>
+            <textarea className='text-inputs' name={name} placeholder={placeholder} value={value} onChange={onchange}/>
         </label>
     </div>
   )

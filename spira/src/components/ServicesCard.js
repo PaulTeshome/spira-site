f65c0ee27f3fb8697811c14f175daf73d5d36330
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-24 01:21:12 +0300

    Update ServicesCard.js
    add animation to card

--- spira/src/components/ServicesCard.js
+++ spira/src/components/ServicesCard.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { motion } from 'framer-motion'
 
 function ServicesCard({service_name:name,service_desc:desc}) {
+  const [isOpen, openCard]= useState(false)
+
+  const handleOpening= () => {
+    openCard(prevState => !prevState)
+    console.log(isOpen)
+  }
   return (
     <div>
         <div className='services-card'>
             <span className='services-name'>{name}</span>
-            <button className='dropdown-btn'></button>
+            <motion.button className='dropdown-btn' onClick={handleOpening}
+                animate={{
+                  rotate: isOpen?180:0,
+                }}
+
+                transition={{
+                  ease: 'easeOut'
+                }}
+
+            ></motion.button>
         </div>
-        <div className='services-desc-card'>
-        its desc {desc}
-        </div>
+        <motion.div className='services-desc-card'
+          animate={{
+            y:isOpen?0:-30,
+            height: isOpen?'auto':'0'
+          }}
+
+          initial={{
+            height: '0',
+            y:0
+          }}
+          transition={{
+            ease: 'easeOut'
+          }}
+        >
+           {desc}
+        </motion.div>
     </div>
   
   )

fb295f6efc2f03e629a5e1253d8609af94694eda
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-23 10:30:42 +0300

    Update HireForm.js

--- spira/src/components/HireForm.js
+++ spira/src/components/HireForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './HireForm.css'
 import InputComponent from './InputComponent'
 import TextInput from './TextInput'
@@ -6,27 +6,51 @@ import FixedHeader from './FixedHeader'
 import RadioComponent from './RadioComponent'
 
 function HireForm() {
+
+  const [inputs,setInputs]= useState({
+    fname:'',
+    lname:'',
+    email:'',
+    cname:'',
+    phone:'',
+    services:'marketing',
+    spec:''
+  })
+
+  const changeInputs= e=>{
+
+    setInputs({
+      ...inputs,
+    [e.target.name]: e.target.value
+    })
+  }
+
+  const submitInputs= event=>{
+    alert(JSON.stringify(inputs))
+    event.preventDefault()
+  }
+
   return (
     <div className='hire-form-container'>
       <FixedHeader logo_link="/#home" services="/#services" about="/#abtUs" hireUs=""/>
       <div className='form-container' id="top">
-        <form className='hire-form'>
-            <InputComponent label="First Name" type="text" id="fname" name="fname" placeholder='Enter First Name' classNm='form-inputs'/>
-            <InputComponent label="Last Name" type="text" id="lname" name="lname" placeholder='Enter Last Name' classNm='form-inputs'/>
-            <InputComponent  label="Email" type="Email" id="email" name="email" placeholder='Enter your email' classNm='form-inputs'/>
-            <InputComponent label="Company Name" type="text" id="cname" name="cname" placeholder='Enter Company Name' classNm='form-inputs'/>
-            <InputComponent label="Phone" type="tel" id="phone" name="phone" placeholder='Enter Phone Number' classNm='form-inputs'/>
+        <form className='hire-form' onSubmit={submitInputs}>
+            <InputComponent label="First Name" type="text" id="fname" name="fname" placeholder='Enter First Name' classNm='form-inputs' value={inputs.fname} onchange={changeInputs}/>
+            <InputComponent label="Last Name" type="text" id="lname" name="lname" placeholder='Enter Last Name' classNm='form-inputs' value={inputs.lname} onchange={changeInputs} />
+            <InputComponent  label="Email" type="Email" id="email" name="email" placeholder='Enter your email' classNm='form-inputs' value={inputs.email} onchange={changeInputs}/>
+            <InputComponent label="Company Name" type="text" id="cname" name="cname" placeholder='Enter Company Name' classNm='form-inputs' value={inputs.cname} onchange={changeInputs}/>
+            <InputComponent label="Phone" type="tel" id="phone" name="phone" placeholder='Enter Phone Number' classNm='form-inputs' value={inputs.phone} onchange={changeInputs}/>
             
             <label>
                Tell us how we could be of help
             </label><br/>
             
-            <RadioComponent value="marketing" label="Marketing" checked={true}/>
-            <RadioComponent value="accMgmt" label="Accounting Management" checked={true}/>
-            <RadioComponent value="content" label="Content Creation" checked={true}/>
-            <RadioComponent value="other" label="Other" checked={true}/>
+            <RadioComponent value="marketing" label="Marketing" name="services" checked={inputs.services === 'marketing'}  onchange={changeInputs}/>
+            <RadioComponent value="accMgmt" label="Accounting Management" name="services" checked={inputs.services === 'accMgmt'}  onchange={changeInputs}/>
+            <RadioComponent value="content" label="Content Creation" name="services" checked={inputs.services === 'content'}  onchange={changeInputs}/>
+            <RadioComponent value="other" label="Other" name="services" checked={inputs.services === 'other'}  onchange={changeInputs}/>
             
-            <TextInput textLabel="Specify" name="spec"  placeholder='Specify your choice above'/>
+            <TextInput textLabel="Specify" name="spec"  placeholder='Specify your choice above' value={inputs.spec} onchange={changeInputs}/>
 
             <InputComponent type="submit" name="submit" value="Submit" classNm='form-inputs'/>
         </form>

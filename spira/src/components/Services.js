3ef4b0e6583b9c66015adde17fedd71cc4b7c1a9
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-04-24 22:36:15 +0300

    Update Services.js

--- spira/src/components/Services.js
+++ spira/src/components/Services.js
@@ -5,10 +5,10 @@ import ServicesCard from './ServicesCard'
 function Services() {
   return (
     // services.map((service)=>{
-    //     <ServicesCard service_name={service.name}service_desc="we manage social media"/>
+    //     <ServicesCard service_name={service.name} service_desc={service.description}/>
     // })
     <div className='services-container' id='services'>
-        <span>OUR SERVICES</span>
+        <span className='services-title'>OUR SERVICES</span>
         <ServicesCard service_name="SOCIAL MEDIA MANAGMENT" service_desc="we manage social media"/>
         <ServicesCard service_name="SOCIAL MEDIA MANAGMENT" service_desc="we manage social media"/>
         <ServicesCard service_name="SOCIAL MEDIA MANAGMENT" service_desc="we manage social media"/>

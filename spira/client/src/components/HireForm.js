6641959467e567897d8410c670b2de8d05b86d57
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-05-12 18:33:30 +0300

    Update HireForm.js

--- spira/client/src/components/HireForm.js
+++ spira/client/src/components/HireForm.js
@@ -1,13 +1,18 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import {FormProvider, useForm} from 'react-hook-form'
+import { motion } from 'framer-motion'
 import {DevTool} from '@hookform/devtools'
 import './HireForm.css'
 import InputComponent from './InputComponent'
 import TextInput from './TextInput'
 import FixedHeader from './FixedHeader'
 import RadioComponent from './RadioComponent'
+import { email_validation, name_validation, phone_validation, text_validation } from './utils/inputValidations'
+import {BsFillCheckSquareFill} from 'react-icons/bs'
 
 function HireForm() {
+
+  const [success,setSuccess]= useState(false)
   useEffect(()=>{
     document.title='Hire Us '
   },[])
@@ -22,6 +27,7 @@ function HireForm() {
 
   const submitInputs= handleSubmit((data)=>{
     console.log('inputs',data)
+    setSuccess(true)
   })
 
   return (
@@ -30,24 +36,33 @@ function HireForm() {
       <div className='form-container' id="top">
         <FormProvider {...methods}>
           <form className='hire-form' onSubmit={e => e.preventDefault()} noValidate>
-              <InputComponent label="First Name" type="text" id="fname" name="fname" placeholder='Enter First Name' classNm='form-inputs' />
-              <InputComponent label="Last Name" type="text" id="lname" name="lname" placeholder='Enter Last Name' classNm='form-inputs'/>
-              <InputComponent  label="Email" type="Email" id="email" name="email" placeholder='Enter your email' classNm='form-inputs' />
-              <InputComponent label="Company Name" type="text" id="cname" name="cname" placeholder='Enter Company Name' classNm='form-inputs' />
-              <InputComponent label="Phone" type="tel" id="phone" name="phone" placeholder='Enter Phone Number' classNm='form-inputs'/>
+              <InputComponent label="First Name" type="text" id="fname" name="fname" placeholder='Enter First Name' classNm='form-inputs' {...name_validation}/>
+              <InputComponent label="Last Name" type="text" id="lname" name="lname" placeholder='Enter Last Name' classNm='form-inputs' {...name_validation}/>
+              <InputComponent  label="Email" type="Email" id="email" name="email" placeholder='Enter your email' classNm='form-inputs' {...email_validation}/>
+              <InputComponent label="Company Name" type="text" id="cname" name="cname" placeholder='Enter Company Name' classNm='form-inputs' {...name_validation}/>
+              <InputComponent label="Phone" type="text" id="phone" name="phone" placeholder='Enter Phone Number (+46762727223)' classNm='form-inputs' {...phone_validation}/>
               
               <label>
                 Tell us how we could be of help
-              </label><br/>
-              
+              </label>              
               <RadioComponent value="marketing" label="Marketing" name="services" />
               <RadioComponent value="accMgmt" label="Accounting Management" name="services" />
               <RadioComponent value="content" label="Content Creation" name="services"/>
               <RadioComponent value="other" label="Other" name="services"/>
               
-              <TextInput textLabel="Specify" name="spec"  placeholder='Specify your choice above' required={false}/>
-
+              <TextInput textLabel="Specify" name="spec"  placeholder='Specify your choice above' {...text_validation}/>
+              {success && (
+                <motion.p className="success-msg"
+                  initial= {{ opacity: 0, y: 10 }}
+                  animate= {{ opacity: 1, y: 0 }}
+                  exit= {{ opacity: 0, y: 10 }}
+                  transition= {{ duration: 0.5 }}
+                >
+                  <BsFillCheckSquareFill /> Form has been submitted successfully
+                </motion.p>
+              )}
               <button  className='form-btn' onClick={submitInputs}>Submit</button>
+              
           </form>
         </FormProvider>
        

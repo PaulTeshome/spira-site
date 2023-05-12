4e88c7fcc20e19daed375b52e159b8432d78dd33
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-05-12 18:32:00 +0300

    Update TextInput.js

--- spira/client/src/components/TextInput.js
+++ spira/client/src/components/TextInput.js
@@ -1,22 +1,39 @@
 import React from 'react'
-import { useFormContext } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form'
+import isFormInvalid from './utils/isFormInvalid'
+import findInputError from './utils/findInputError'
+import { AnimatePresence } from 'framer-motion'
+import InputError from './InputError';
 import './TextInput.css'
 
-function TextInput({textLabel,name,placeholder, required}) {
+function TextInput({textLabel,name,placeholder, validation}) {
 
-  const {register} =useFormContext();
+  const {register, formState: { errors },
+} = useFormContext()
+
+const inputError = findInputError(errors, name)
+const isInvalid = isFormInvalid(inputError)
   return (
     <div className='text-input-holder'>
-        <label className='text-input-label'>
-            {textLabel}<br/>
-            <textarea className='text-inputs' placeholder={placeholder} 
-            {...register(name,{
-              required:{
-                value:required,
-                message: 'Field is required'
-              }
-            })}/>
+       <div className='label-info'>
+        <label htmlFor={name} className='text-input-label'>
+            {textLabel}
         </label>
+
+        <AnimatePresence mode="wait" initial={false}>
+           {isInvalid && ( 
+            <InputError
+                message={inputError.error.message}
+                key={inputError.error.message}
+              />
+            )            
+            }
+    
+        </AnimatePresence>
+      </div>
+            <textarea className='text-inputs' placeholder={placeholder} 
+            {...register(name,validation)}/>
+       
     </div>
   )
 }

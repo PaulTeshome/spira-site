ec8981f1d2c37a448a9b73b5e8f7df7f3b2589d4
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-05-13 20:42:40 +0300

    Update Login.js

--- spira/client/src/Login.js
+++ spira/client/src/Login.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import  {Route, Routes} from "react-router-dom"
 import './Login.css'
+import LoginForm from './components/LoginForm'
+import ForgotPsd from './components/ForgotPsd'
 
 function Login() {
   return (
-    <div>
-         this is login
+    <div className='login-page'>
+      
+      <div className='login-card'>
+        <Routes>
+          <Route exact path="*" element={<LoginForm/>}/>
+          <Route exact path="*/forgotpsd/*" element={<ForgotPsd/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }

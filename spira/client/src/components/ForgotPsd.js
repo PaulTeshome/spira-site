ab3d1bf4d0d8fe524e46e3fd96d02891bc1ae034
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-05-14 00:38:02 +0300

    Update ForgotPsd.js

--- spira/client/src/components/ForgotPsd.js
+++ spira/client/src/components/ForgotPsd.js
@@ -3,36 +3,49 @@ import {FormProvider, useForm} from 'react-hook-form'
 import InputComponent from './InputComponent'
 import { forgot_psd_validation} from './utils/inputValidations'
 import { HashLink as Link} from 'react-router-hash-link'
+import { motion } from 'framer-motion'
 import './ForgotPsd.css'
+import { BsFillCheckSquareFill } from 'react-icons/bs'
 
 function ForgotPsd() {
-    const [success,setSuccess]= useState(false)
+    const [submitSuccess,setSubmitSuccess]= useState(false)
+    const [failure,setFailure]= useState(false)
 
     useEffect(()=>{
       document.title='Forgot Password'
     },[])
   
-    const methods= useForm({
-      defaultValues:{
-        services:'marketing'
-      }
-    })
+    const methods= useForm()
   
     const {handleSubmit}=methods
   
     const submitInputs= handleSubmit((data)=>{
       console.log('inputs',data)
-      setSuccess(true)
-      success?console.log("success"):console.log("not success")
+      setSubmitSuccess(true)
+      setFailure(false)
     })
 
+    const success_msg_class= failure?"login-error-msg":"success-msg"
+    const success_msg= failure?"No such username or email is registered!":"Successful redirecting..."
+    const checkLogo=failure?"":<BsFillCheckSquareFill/> 
 
   return (
     <div className='login-form-container'>
         <span className='login-form-title'>Forgot Password</span>
         <FormProvider {...methods}>
           <form className='login-form' onSubmit={e => e.preventDefault()} noValidate>
-              <InputComponent label="Insert Email or Username" type="email" id="email" name="email" placeholder='Enter your registered email or Username' classNm='form-inputs' {...forgot_psd_validation}/>
+          {submitSuccess && (
+                <motion.p className={success_msg_class}
+                  initial= {{ opacity: 0, y: 10 }}
+                  animate= {{ opacity: 1, y: 0 }}
+                  exit= {{ opacity: 0, y: 10 }}
+                  transition= {{ duration: 0.5 }}
+                >
+                  
+                  {checkLogo}&nbsp;{success_msg}
+                </motion.p>
+              )}
+              <InputComponent label="Insert Email or Username" type="text" id="usrData" name="usrData" placeholder='Enter your registered email or Username' classNm='form-inputs' {...forgot_psd_validation}/>
             
               <button  className='login-btn' onClick={submitInputs}>Submit</button>
               

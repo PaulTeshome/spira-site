3c48ca92d66a1b3e416c4d17f41969db1836d219
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-05-14 14:08:04 +0300

    Update CustomizationPage.js

--- spira/client/src/components/CustomizationPage.js
+++ spira/client/src/components/CustomizationPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './CustomizationPage.css'
-import SettingCard from './SettingCard'
+import  {Route, Routes} from "react-router-dom"
+import CustomSettingChoice from './CustomSettingChoice'
+import GeneralPageSettings from './GeneralPageSettings'
 
 function CustomizationPage() {
   return (
@@ -8,11 +10,14 @@ function CustomizationPage() {
       <span className='customization-title'> Website Customization Settings</span>
       <span className='customization-intro'>Welcome to website customization settings! Here you can modify specific contents of your website to your desire.
                                              Please select from the options below to proceed accordingly.</span>
-        <SettingCard title="General Page Elements" desc="Edit elements like email, phone numbers, location of company, introduction motto..." link="/"/>
-        <SettingCard title="Our Services list" desc="Add, edit or remove the services listed in the OUR SERVICES section of the website" link="/"/>
-        <SettingCard title="Recent Works list" desc="Add,edit or remove the recent projects listed in the RECENT WORKS section of the website" link="/"/>
-        <SettingCard title="Team Member" desc="Add, edit or remove team member information listed in the OUR TEAM section of the website" link="/"/>
-        <SettingCard title="Testimonials" desc="Add, edit or remove customer testimonials" link="/"/>
+        <div className='customization-body' >
+        <Routes>
+            <Route exact path="*" element={<CustomSettingChoice/>}/>
+            <Route exact path="*/generalpage" element={<GeneralPageSettings/>}/>
+        </Routes>
+
+        </div>                       
+    
     </div>
   )
 }

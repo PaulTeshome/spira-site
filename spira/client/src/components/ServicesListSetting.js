3a30bdf7eb549b862dff784a43ca71d892b9b207
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-05-15 00:47:20 +0300

    Create ServicesListSetting.js

---
+++ spira/client/src/components/ServicesListSetting.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ServicesListSetting.css'
+import ServicesSettingCard from './ServicesSettingCard'
+
+function ServicesListSetting() {
+  return (
+    <div className='services-settings-holder'>
+        <span className='services-settings-title'> Services List Edit Form</span>
+        <ServicesSettingCard service_id={"2"}   service_name="name one" service_description="desc one"/>
+
+    </div>
+  )
+}
+
+export default ServicesListSetting

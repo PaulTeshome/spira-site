a3a355ad47881fab44dbaa31366f4098421c0272
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-05-14 14:08:23 +0300

    Update InputComponent.js

--- spira/client/src/components/InputComponent.js
+++ spira/client/src/components/InputComponent.js
@@ -6,7 +6,7 @@ import isFormInvalid from './utils/isFormInvalid'
 import findInputError from './utils/findInputError'
 import InputError from './InputError';
 
-function InputComponent({label, type, name, id, placeholder,classNm,validation}) {
+function InputComponent({label, type, name, id, placeholder,classNm,validation, disabled}) {
 
   const {register, formState: { errors },
         } = useFormContext()
@@ -32,7 +32,7 @@ function InputComponent({label, type, name, id, placeholder,classNm,validation})
         </AnimatePresence>
       </div>
        
-        <input type={type} id={id} placeholder={placeholder} className={classNm} 
+        <input type={type} id={id} placeholder={placeholder} className={classNm} disabled={disabled}
           {...register(name, validation)}
 
         /><br/>

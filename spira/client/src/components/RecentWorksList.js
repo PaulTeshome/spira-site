1052bfaf0d585917fc82bd04b44690c49f7c1451
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 18:39:36 +0300

    Update RecentWorksList.js

--- spira/client/src/components/RecentWorksList.js
+++ spira/client/src/components/RecentWorksList.js
@@ -6,8 +6,7 @@ function RecentWorksList() {
   return (
     <div className='recent-settings-holder'>
     <span className='recent-settings-title'> Recent Works List Edit Form</span>
-    <RecentWorksListCard project_title="proj 1"  project_id={1} project_description="this is the desc of proj 1" project_image="image"/>  
-    <RecentWorksListCard project_title="proj 1"  project_id={1} project_description="this is the desc of proj 1" project_image="image"/>  
+    <RecentWorksListCard key="1" project_id={1} project_title="proj 1"   project_description="this is the desc of proj 1" project_image="image"/>  
 
 </div>
   )

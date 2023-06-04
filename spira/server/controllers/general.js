c732955931183de4b3d3221e934844191b669af3
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 13:59:01 +0300

    Create general.js

---
+++ spira/server/controllers/general.js
@@ -0,0 +1,40 @@
+import { db } from "../db.js";
+
+export const getMotto = (req, res) => {
+    const q= "select comp_motto,comp_mission from main_data where data_id=1"
+
+    db.query(q, (err,data) => {
+
+        if (err) {
+            res.json(err)
+        }else {
+            res.json(data)
+        }
+    });
+}
+
+export const getFooter = (req, res) => {
+    const q= "select * from main_data where data_id=1"
+
+    db.query(q, (err,data) => {
+
+        if (err) {
+            res.json(err)
+        }else {
+            res.json(data)
+        }
+    });
+}
+
+export const getAbout = (req, res) => {
+    const q= "select comp_abt from main_data where data_id=1"
+
+    db.query(q, (err,data) => {
+
+        if (err) {
+            res.json(err)
+        }else {
+            res.json(data)
+        }
+    });
+}

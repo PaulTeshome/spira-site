7101ed75539265455fec900b79f934ba16df5a0d
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 18:39:23 +0300

    Update MemberCard.js

--- spira/client/src/components/MemberCard.js
+++ spira/client/src/components/MemberCard.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-function MemberCard({name,title,source}) {
+function MemberCard({id,name,title,imgName}) {
+  const images = require.context('../images', true)
+
+  let imgUrl;
+    if(imgName!==null) {
+      // console.log("is not null")
+      imgUrl= images(`./${imgName}`)
+    }else{
+      imgUrl= images("./default_profile.png")
+    }
   return (
     <div className='member-card'>
         <div className='member-profile-holder'>
-            <img className='member-profile' src={source} alt='member profile'/>
+            <img className='member-profile' src={imgUrl} alt='member profile'/>
         </div>
         <span className='member-name'>{name}</span>
         <span className='member-title'>{title}</span>

72505d28fc9c7526376124210fee0155f7abe030
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 13:10:22 +0300

    Update Home.js

--- spira/client/src/components/Home.js
+++ spira/client/src/components/Home.js
@@ -18,7 +18,7 @@ function Home() {
   useEffect(()=>{
     document.title='Spira Agency'
 
-    axios.get("http://localhost:8800/getMotto")
+    axios.get("/getMotto")
     .then(res => {
       setMotto(res.data[0].comp_motto)
         setMission(res.data[0].comp_mission)

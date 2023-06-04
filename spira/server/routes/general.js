13f3c49a7ccbab35b6c56a95d22db9125fd92a94
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 18:40:32 +0300

    Update general.js

--- spira/server/routes/general.js
+++ spira/server/routes/general.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { getFooter,updateFooter, getMotto, updateMotto, getAbout,updateAbout } from "../controllers/general.js";
+import { getFooter, getMotto,  getAbout,updateAll, getAll } from "../controllers/general.js";
 
 const router = express.Router();
 
 router.get('/getMotto', getMotto);
-router.put('/updateMotto', updateMotto);
-
 router.get('/getFooter', getFooter);
-router.put('/updateFooter', updateFooter);
-
 router.get('/getAbout', getAbout);
-router.put('/updateAbout', updateAbout);
+router.get('/getAll', getAll);
+router.put('/updateAll', updateAll);
+
+
+
 
 export default router

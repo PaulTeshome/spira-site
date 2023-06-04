1497a4398d7e822fd241866f703325efacc433c8
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 13:59:17 +0300

    Update index.js

--- spira/server/index.js
+++ spira/server/index.js
@@ -1,5 +1,7 @@
 import express, { json } from "express";
-import {db} from './db.js'
+import serviceRoutes from "./routes/services.js";
+import generalRoutes from "./routes/general.js";
+import projectRoutes from "./routes/projects.js";
 
 import cors from "cors";
 
@@ -13,61 +15,16 @@ app.use(cors({
     credentials: true
 }));
 
+
+app.use('/api/services', serviceRoutes)
+app.use('/api/general', generalRoutes)
+app.use('/api/projects', projectRoutes)
+
+
 app.get('/', (req, res) => {
     res.json("success");
 })
 
-app.get('/getServices', (req, res) => {
-    const q= "select * from services"
-
-    db.query(q, (err,data) => {
-
-        if (err) {
-            res.json(err)
-        }else {
-            res.json(data)
-        }
-    });
-})
-
-app.get('/getMotto', (req, res) => {
-    const q= "select comp_motto,comp_mission from main_data where data_id=1"
-
-    db.query(q, (err,data) => {
-
-        if (err) {
-            res.json(err)
-        }else {
-            res.json(data)
-        }
-    });
-})
-
-app.get('/getProjects', (req, res) => {
-    const q= "select * from projects"
-
-    db.query(q, (err,data) => {
-
-        if (err) {
-            res.json(err)
-        }else {
-            res.json(data)
-        }
-    });
-})
-
-app.get('/getFooter', (req, res) => {
-    const q= "select * from main_data where data_id=1"
-
-    db.query(q, (err,data) => {
-
-        if (err) {
-            res.json(err)
-        }else {
-            res.json(data)
-        }
-    });
-})
 
 app.listen(8800, () => {
     console.log("Server running on port 8800...")

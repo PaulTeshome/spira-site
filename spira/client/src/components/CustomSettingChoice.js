eab93a5d454d3e3e2c0537885119884cf0bc1c7d
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 18:39:05 +0300

    Update CustomSettingChoice.js

--- spira/client/src/components/CustomSettingChoice.js
+++ spira/client/src/components/CustomSettingChoice.js
@@ -8,8 +8,8 @@ function CustomSettingChoice() {
     <SettingCard title="General Page Elements" desc="Edit elements like email, phone numbers, location of company, introduction motto..." link="*/generalpage"/>
     <SettingCard title="Our Services list" desc="Add, edit or remove the services listed in the OUR SERVICES section of the website" link="*/servicessetting"/>
     <SettingCard title="Recent Works list" desc="Add,edit or remove the recent projects listed in the RECENT WORKS section of the website" link="*/recentsetting"/>
-    <SettingCard title="Team Member" desc="Add, edit or remove team member information listed in the OUR TEAM section of the website" link="/"/>
-    <SettingCard title="Testimonials" desc="Add, edit or remove customer testimonials" link="/"/>
+    <SettingCard title="Team Member" desc="Add, edit or remove team member information listed in the OUR TEAM section of the website" link="*/teamsetting"/>
+    <SettingCard title="Testimonials" desc="Add, edit or remove customer testimonials" link="*/testimonialsetting"/>
     </div>
   )
 }

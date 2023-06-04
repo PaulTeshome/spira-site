83325c65ad6cda2038730cf8887055b429cd51bc
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 03:00:09 +0300

    Update Home.js

--- spira/client/src/components/Home.js
+++ spira/client/src/components/Home.js
@@ -1,28 +1,38 @@
 // import logo from '../images/spira_logo1 1.png';
 import './styles/Home.css';
-import { useEffect } from 'react';
+import { useEffect ,useState} from 'react';
 import LandingHeader from './LandingHeader';
 import MottoSection from './MottoSection';
 import Services from './Services';
 import RecentWorks from './RecentWorks';
 import OurTeam from './OurTeam';
 import AboutUs from './AboutUs';
+import axios from 'axios';
 
 
 function Home() {
 
+  const [motto,setMotto]=useState("")
+  const [mission,setMission]=useState("");
+
   useEffect(()=>{
     document.title='Spira Agency'
+
+    axios.get("http://localhost:8800/getMotto")
+    .then(res => {
+      setMotto(res.data[0].comp_motto)
+        setMission(res.data[0].comp_mission)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   },[])
 
   return (
     <div>
       <LandingHeader/>
-      <MottoSection motto="EMPOWERING YOUR BRAND IN THE DIGITAL SPACE" description="
-                At Spira, our mission is to provide personalized and result-driven solutions
-                to help our clients achieve their business goals. 
-                We believe in building strong relationships with our clients 
-                and working closely with them to understand their unique needs and challenges."/>
+      <MottoSection motto={motto} description={mission}/>
       <Services/>
       <RecentWorks/>
       <OurTeam/>

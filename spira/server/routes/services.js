67fb90c982f1fef1b70f5301cc98624169848660
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 14:09:09 +0300

    Update services.js

--- spira/server/routes/services.js
+++ spira/server/routes/services.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getServices } from "../controllers/services.js";
+import { getServices, updateService, addService,deleteService } from "../controllers/services.js";
 
 const router = express.Router();
 
 router.get('/getServices', getServices);
+router.put('/updateService', updateService);
+router.delete('/deleteService', deleteService);
+router.post('/addService', addService);
 
 export default router

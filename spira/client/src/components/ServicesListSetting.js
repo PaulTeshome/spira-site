f118c92e461d2a0835d1331e62fc2e754463b8c9
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 18:39:45 +0300

    Update ServicesListSetting.js

--- spira/client/src/components/ServicesListSetting.js
+++ spira/client/src/components/ServicesListSetting.js
@@ -6,7 +6,7 @@ function ServicesListSetting() {
   return (
     <div className='services-settings-holder'>
         <span className='services-settings-title'> Services List Edit Form</span>
-        <ServicesSettingCard service_id={"2"}   service_name="name one" service_description="desc one"/>
+        <ServicesSettingCard key={"2"} service_id={"2"}   service_name="name one" service_description="desc one"/>
 
     </div>
   )

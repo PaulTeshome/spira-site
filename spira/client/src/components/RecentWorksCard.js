0ef513cac622b3c533c41885881aab0d9c2b6625
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-04 18:39:28 +0300

    Update RecentWorksCard.js

--- spira/client/src/components/RecentWorksCard.js
+++ spira/client/src/components/RecentWorksCard.js
@@ -1,37 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/RecentWorksCard.css'
 
 function RecentWorksCard({proj_title, proj_desc, imgName}) {
   const images = require.context('../images', true)
-  let img;
+  
+  
+  let imgUrl;
     if(imgName!==null) {
-      img= images(imgName)
-      console.log(imgName)
+      imgUrl= images(`./${imgName}`)
+    }else{
+      imgUrl= images("./stockholm2.jpg")
     }
-    else{
-      console.log("f",imgName)
-      
-      img= images("./stockholm2.jpg")
+
+    const [cardStyle,updateCardStyle] = useState({ backgroundImage: `url(${imgUrl})` })
+    const gradient = 'linear-gradient(to right, rgba(0, 0, 0, 0.729),rgba(0, 0, 0, 0.729), rgba(0, 0, 2, 0.52)),';
+
+    const hoverEffect=()=>{
+      updateCardStyle({ backgroundImage:  `${gradient}url('${imgUrl}')` })
     }
-    
+
+    const leaveEffect=()=>{
+      updateCardStyle({ backgroundImage: `url(${imgUrl})` })
+    }
 
   return (
     <>
-      <style>
-          {
-            `
-            .recent-card{
-              background-image: url('${img}');
-            }
-
-            .recent-card:hover{
-              transform: scale(1.04,1.04);
-              background-image:  linear-gradient(to right, rgba(0, 0, 0, 0.729),rgba(0, 0, 0, 0.729), rgba(0, 0, 2, 0.52)), url('${img}');
-            } 
-            `
-            }
-          </style>
-      <div className='recent-card'>
+   
+      <div className='recent-card' style={cardStyle} onMouseEnter={hoverEffect} onMouseLeave={leaveEffect}>
+      <div className="card-overlay" style={{ backgroundImage: `${gradient}url(${imgUrl})` }}></div>
           <div className='recent-card-desc'>
             <span className='recent-card-header'>{proj_title}</span>
             <span className='recent-card-text'>{proj_desc}</span>

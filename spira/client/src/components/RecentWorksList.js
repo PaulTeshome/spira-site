70870eaa3d2b63068ea4e367d05fad214d20ad56
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-05 14:54:32 +0300

    Update RecentWorksList.js

--- spira/client/src/components/RecentWorksList.js
+++ spira/client/src/components/RecentWorksList.js
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles/RecentWorksList.css'
 import RecentWorksListCard from './RecentWorksListCard'
+import axios from 'axios'
+import NewProjectCard from './NewProjectCard'
 
 function RecentWorksList() {
+  const [newProject,setNewProject] = useState(false)
+  const [newRender,setRender] = useState(false)
+
+  const [projects, setProjects] = useState([]);
+
+  const addProject = () => {
+    setNewProject(true)
+  }
+
+  const closeNewProjectCard = () => {
+    setNewProject(false);
+  };
+
+  useEffect(() =>{
+    axios.get("/projects/getProjects")
+    .then(res => {
+      setProjects(res.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  },[newProject,newRender])
+
+  const updateList=() => {
+    setRender(!newRender)
+  }
+
   return (
     <div className='recent-settings-holder'>
     <span className='recent-settings-title'> Recent Works List Edit Form</span>
-    <RecentWorksListCard key="1" project_id={1} project_title="proj 1"   project_description="this is the desc of proj 1" project_image="image"/>  
 
-</div>
+        {newProject?
+        
+        <NewProjectCard onClose={closeNewProjectCard}/>
+        :
+        <>
+        <button className='service-setting-edit-btn' onClick={addProject}>Add new Project</button>
+        {
+        projects.map((project)=>{
+          return(
+          <RecentWorksListCard key={project.project_id} project_id={project.project_id}  project_title={project.project_title}    project_description={project.project_description}  update={updateList}/>  
+          )
+          })
+        }
+        </>
+        }
+        
+
+    </div>
   )
 }
 

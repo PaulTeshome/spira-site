d31e5b58ac92c63bca46b2a4857e4fa80e4bf28a
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-05 18:49:11 +0300

    Update RecentWorksListCard.js

--- spira/client/src/components/RecentWorksListCard.js
+++ spira/client/src/components/RecentWorksListCard.js
@@ -1,17 +1,32 @@
-import React,{useEffect, useState} from 'react'
+import React,{useState} from 'react'
 import {FormProvider, useForm} from 'react-hook-form'
 import { motion } from 'framer-motion'
 import InputComponent from './InputComponent'
 import TextInput from './TextInput'
 import { text_validation } from './utils/inputValidations'
-import {BsFillCheckSquareFill} from 'react-icons/bs'
+import {BsFillCheckSquareFill, BsFillXSquareFill} from 'react-icons/bs'
 import './styles/RecentWorksListCard.css'
+import axios from 'axios'
 
-function RecentWorksListCard({project_id,project_title, project_description,update}) {
-    const [successMsg,setSuccessMsg]= useState(false)
-    const [disabled,setDisabled]= useState(true)
-    const [editor,setEditor]= useState(true)
-    const [image,setImage]= useState('')
+function RecentWorksListCard({project_id,project_title, project_description,old_image,update}) {
+
+  const [successMsg,setSuccessMsg]= useState(false)
+  const [deleteMsg,setDeleteMsg]= useState(false)
+  const [errorMsg,setErrorMsg]= useState(false)
+  const [errorData,setErrorData]= useState(" ")
+  const [disabled,setDisabled]= useState(true)
+  const [editor,setEditor]= useState(true)
+  const [image,setImage]= useState({file:[]})
+
+
+  const methods= useForm({
+    defaultValues:{
+      project_title:project_title,
+      project_description:project_description,
+    }
+  })
+  
+  const {handleSubmit}=methods
 
     const disabledTxt = disabled?"disabled":""
 
@@ -20,55 +35,102 @@ function RecentWorksListCard({project_id,project_title, project_description,upda
         setEditor(false)
     }
 
-    const deleteForm= ()=>{
-        let confDel= window.confirm("Are you sure you want to delete project data?")
+    const deleteForm= handleSubmit((data)=>{
+      let confDel= window.confirm(`Are you sure you want to delete ${project_title}?`)
 
-        if (confDel){
-          alert("Delete")
-        }else{
-          alert("no delete")
-        }
-    }
-
-    function handleImage(e){
-
-      console.log(e.target.files+"here are the files")
-      setImage(e.target.files[0])
-    }
-
-    useEffect(()=>{
-      document.title='Admin Dashboard'
-      
-    },[])
-  
-    const methods= useForm({
-      defaultValues:{
-        project_title:project_title,
-        project_description:project_description,
+      if (confDel){
+        axios.delete("/projects/deleteProject", { data: { project_id: project_id, old_image: old_image}})
+        .then(res => {
+          setDeleteMsg(true)
+          setTimeout(()=>{
+            setDeleteMsg(false)
+            update()
+          },2000)
+          
+        })
+        .catch(error => {
+          console.error('Error deleting value:', error);
+          const errorMessage = error.response ? error.response.data.error : 'Failed to delete value. Please Try again.';
+          setErrorMsg(true)
+          setErrorData(errorMessage)
+          setTimeout(()=>{
+            setErrorMsg(false)
+            setEditor(true)
+            setDisabled(true)
+            update()
+            },3000)
+        });
+      }else{
+        setEditor(true)
+        setDisabled(true)
+        
       }
     })
-    
-    const {handleSubmit}=methods
-  
+
+     
     const submitInputs= handleSubmit((data)=>{
-      const imageData= new FormData();
-      imageData.append('project_image',image)
-      const newData={...data,project_id,imageData}
-      console.log("image name ",image)
-      console.log('inputs',newData)
-      setSuccessMsg(true)
-      setEditor(true)
-      setDisabled(true)
-      setTimeout(()=>{setSuccessMsg(false)},2000)
-    })
-    
+        
+      const formData = new FormData();
+      formData.append('project_id', project_id);
+      formData.append('project_title', data.project_title);
+      formData.append('project_description', data.project_description);
+      formData.append('old_image',old_image);
+      formData.append('project_image', image.file);
+      
+      
+
+      axios.put("/projects/updateProject", formData,
+      {   
+          headers: { "Content-Type": "multipart/form-data" } 
+      })
+      .then(res => {
+          setSuccessMsg(true)
+          setTimeout(()=>{
+                        setSuccessMsg(false)
+                        setEditor(true)
+                        setDisabled(true)
+                        update();
+                    },2000)
+          
+      })
+      .catch(error => {
+        console.error('Error updating value:', error);
+        const errorMessage = error.response ? error.response.data.error : 'Failed to update value. Please Try again.';
+        setErrorMsg(true)
+        setErrorData(errorMessage)
+        setTimeout(()=>{
+          setErrorMsg(false)
+          setEditor(true)
+          setDisabled(true)
+           update();
+          },5000)
+      });
+
+  })
+
+
+    const handleImage = (event) => {
+      const file = event.target.files[0];
+      const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']; 
+
+      if (allowedTypes.includes(file.type)) {
+          setErrorMsg(false);
+          setImage({...image,file:file});
+      } else {
+          setErrorMsg(true);
+          setErrorData('Please upload a valid image file (JPEG, PNG, or GIF)');
+      }
+
+    };
+
+
   return (
     <div className='recent-setting-card'>
         <FormProvider {...methods}>
           <form className='recent-setting-form' onSubmit={e => e.preventDefault()} noValidate>
             <div className='recent-setting-inputs'>
               <InputComponent label="Project Title" type="text" id="project_title" name="project_title" placeholder='Enter project title...' classNm='form-inputs' disabled={disabledTxt} {...text_validation}/>
-              <InputComponent label="Project Image" type="file" id="project_image" name="project_image" classNm='form-file-inputs' disabled={disabledTxt} onchange={handleImage} />
+              <InputComponent label="Project Image" type="file" id="project_image" name="project_image" classNm='form-file-inputs' disabled={disabledTxt} oninput={handleImage} />
               <TextInput textLabel="Project Description" name="project_description"  placeholder='Enter project description' disabled={disabledTxt} {...text_validation}/>
             </div>
             <div className='recent-setting-controls'>
@@ -81,6 +143,26 @@ function RecentWorksListCard({project_id,project_title, project_description,upda
                     <BsFillCheckSquareFill /> Project updated successfully
                     </motion.p>
                 )}
+
+                {deleteMsg && (
+                    <motion.p className="service-success-msg"
+                        initial= {{ opacity: 0, y: 10 }}
+                        animate= {{ opacity: 1, y: 0 }}
+                        exit= {{ opacity: 0, y: 10 }}
+                        transition= {{ duration: 0.5 , ease: 'easeInOut'}}>
+                    <BsFillCheckSquareFill /> Project deleted successfully
+                    </motion.p>
+                )}
+
+                {errorMsg && (
+                    <motion.p className="error-msg"
+                    initial= {{ opacity: 0, y: 10 }}
+                    animate= {{ opacity: 1, y: 0 }}
+                    exit= {{ opacity: 0, y: 10 }}
+                    transition= {{ duration: 0.5 }}>
+                    <BsFillXSquareFill /> {errorData}
+                    </motion.p>
+                )}  
 
                 {
                 editor && (

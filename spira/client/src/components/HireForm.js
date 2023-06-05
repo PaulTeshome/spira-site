7eddb125dcc6e82ecea19523c6dc464610ee3970
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-06 00:32:03 +0300

    Update HireForm.js

--- spira/client/src/components/HireForm.js
+++ spira/client/src/components/HireForm.js
@@ -9,10 +9,13 @@ import FixedHeader from './FixedHeader'
 import RadioComponent from './RadioComponent'
 import { email_validation, name_validation, phone_validation, text_validation } from './utils/inputValidations'
 import {BsFillCheckSquareFill} from 'react-icons/bs'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function HireForm() {
 
   const [success,setSuccess]= useState(false)
+  const navigate = useNavigate();
   
   useEffect(()=>{
     document.title='Hire Us '
@@ -27,8 +30,16 @@ function HireForm() {
   const { control,handleSubmit}=methods
 
   const submitInputs= handleSubmit((data)=>{
-    console.log('inputs',data)
-    setSuccess(true)
+   
+    axios.post('/hire/makeRequest',data)
+    .then(res=>{
+      setSuccess(true)
+      setTimeout(()=>{setSuccess(false)},3000)
+      setTimeout(()=>{navigate('/')},5000)
+    })
+    .catch(err=>{
+
+    });
   })
 
   return (
@@ -37,21 +48,21 @@ function HireForm() {
       <div className='form-container' id="top">
         <FormProvider {...methods}>
           <form className='hire-form' onSubmit={e => e.preventDefault()} noValidate>
-              <InputComponent label="First Name" type="text" id="fname" name="fname" placeholder='Enter First Name' classNm='form-inputs' {...name_validation}/>
-              <InputComponent label="Last Name" type="text" id="lname" name="lname" placeholder='Enter Last Name' classNm='form-inputs' {...name_validation}/>
+              <InputComponent label="First Name" type="text" id="first_name" name="first_name" placeholder='Enter First Name' classNm='form-inputs' {...name_validation}/>
+              <InputComponent label="Last Name" type="text" id="last_name" name="last_name" placeholder='Enter Last Name' classNm='form-inputs' {...name_validation}/>
               <InputComponent  label="Email" type="Email" id="email" name="email" placeholder='Enter your email' classNm='form-inputs' {...email_validation}/>
-              <InputComponent label="Company Name" type="text" id="cname" name="cname" placeholder='Enter Company Name' classNm='form-inputs' {...name_validation}/>
+              <InputComponent label="Company Name" type="text" id="comp_name" name="comp_name" placeholder='Enter Company Name' classNm='form-inputs' {...name_validation}/>
               <InputComponent label="Phone" type="text" id="phone" name="phone" placeholder='Enter Phone Number (+46762727223)' classNm='form-inputs' {...phone_validation}/>
               
               <label>
                 Tell us how we could be of help
               </label>              
               <RadioComponent value="marketing" label="Marketing" name="services" />
-              <RadioComponent value="accMgmt" label="Accounting Management" name="services" />
-              <RadioComponent value="content" label="Content Creation" name="services"/>
+              <RadioComponent value="accounting management" label="Accounting Management" name="services" />
+              <RadioComponent value="content creation" label="Content Creation" name="services"/>
               <RadioComponent value="other" label="Other" name="services"/>
               
-              <TextInput textLabel="Specify" name="spec"  placeholder='Specify your choice above' {...text_validation}/>
+              <TextInput textLabel="Specify" name="specify"  placeholder='Specify your choice above' {...text_validation}/>
               {success && (
                 <motion.p className="success-msg"
                   initial= {{ opacity: 0, y: 10 }}

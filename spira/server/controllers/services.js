4b03a71b674afa4614a6aadbfde66a3d8ec63c29
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-05 03:27:15 +0300

    Update services.js

--- spira/server/controllers/services.js
+++ spira/server/controllers/services.js
@@ -14,11 +14,31 @@ export const getServices= (req, res) => {
 }
 
 export const addService = (req, res) => {
-    
+    const q= "INSERT INTO services (service_name,service_description) VALUES (?,?)"
+
+    db.query(q,[req.body.service_name,req.body.service_description], (err,data) => {
+
+        if (err) {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to insert value in database. Please try again.' })
+        }else {
+            res.json(data)
+        }
+    });
 }
 
 export const updateService = (req, res) => {
-    
+    const q= "UPDATE services SET service_name=?, service_description=?  WHERE service_id=?"
+
+    db.query(q,[req.body.service_name,req.body.service_description,req.body.service_id], (err,data) => {
+
+        if (err) {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to update value in database. Please try again.' })
+        }else {
+            res.json(data)
+        }
+    });
 }
 
 export const deleteService = (req, res) => {

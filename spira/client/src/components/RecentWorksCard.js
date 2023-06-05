3b1379f88197fc417319221963b63026cf692a9f
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-05 14:54:24 +0300

    Update RecentWorksCard.js

--- spira/client/src/components/RecentWorksCard.js
+++ spira/client/src/components/RecentWorksCard.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react'
 import './styles/RecentWorksCard.css'
 
 function RecentWorksCard({proj_title, proj_desc, imgName}) {
-  const images = require.context('../images', true)
+  const images = require.context('../images/projectBg', true)
   
   
   let imgUrl;
+  try{
     if(imgName!==null) {
       imgUrl= images(`./${imgName}`)
     }else{
       imgUrl= images("./stockholm2.jpg")
     }
+  }catch(error){
+    imgUrl= images("./stockholm2.jpg")
+  }
+   
 
     const [cardStyle,updateCardStyle] = useState({ backgroundImage: `url(${imgUrl})` })
     const gradient = 'linear-gradient(to right, rgba(0, 0, 0, 0.729),rgba(0, 0, 0, 0.729), rgba(0, 0, 2, 0.52)),';

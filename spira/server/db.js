27a6a39711cdb0bd611d18b20201cdfb78233ff3
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-06 16:52:55 +0300

    Update db.js

--- spira/server/db.js
+++ spira/server/db.js
@@ -1,8 +1,11 @@
 import mysql from "mysql";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export  const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'spira_db',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,   
 });

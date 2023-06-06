5185fc779238c4cd9c205a2962b6d24da4bf7083
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-07 00:57:53 +0300

    Update auth.js

--- spira/server/controllers/auth.js
+++ spira/server/controllers/auth.js
@@ -1,13 +1,67 @@
 import { db } from "../db.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+
+
+
 
 export const register= (req,res)=>{
+//   const salt = bcrypt.genSaltSync(10);
+
+// // Hash a password with the salt
+//   const hash = bcrypt.hashSync('password123', salt);
+//   console.log("hash: ",hash)
+
+//   const isMatch = bcrypt.compareSync('password123', hash);
+//   console.log("match: ",isMatch)
+
 
 }
 
 export const login= (req,res)=>{
+
+  const {username,password} = req.body;
+  const q="SELECT * FROM admins WHERE admin_username=?"
+   db.query(q,[username],(err,result)=>{
+
+    if (err) {
+      return res.status(401).json({ message: 'Server Error. Please try again' });
+    }
+    else {
+     
+        const isMatch = bcrypt.compareSync(password, result[0].admin_password);
+
+        if(!isMatch){
+          return res.status(401).json({ message: 'Invalid username or password' })
+        }
+
+        const token = jwt.sign({ username }, 'spira2121', { expiresIn: '10s' });
+        res.cookie('token', token, { httpOnly: true });
+        res.status(200).json({ message: 'Login successful. Redirecting...' });
+    }
+   });
     
 }
 
 export const logout= (req,res)=>{
-    
+
+    res.clearCookie('token');
+    res.status(200).send({ message: 'Logout successful' });
+}
+
+export const checkLogin= (req,res)=>{
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'spira2121');
+    req.user = decoded;
+    return res.status(200).send({ message: 'Welcome', user: req.user , exp:req.exp, iat:req.iat });
+
+  } catch (e) {
+    return res.status(401).send({ message: 'Unauthorized' });
+  }
 }

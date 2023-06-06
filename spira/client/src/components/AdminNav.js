f3bb625147a16b998d214dd901090c73da165015
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-07 00:57:18 +0300

    Update AdminNav.js

--- spira/client/src/components/AdminNav.js
+++ spira/client/src/components/AdminNav.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import './styles/AdminNav.css'
 import AdminNavComp from './AdminNavComp'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 function AdminNav() {
+
+  const navigate= useNavigate()
+
+  const handleLogout=() => {
+    axios.get('/auth/logout')
+    .then((res) => {
+      alert(res.data.message)
+      navigate('/login')
+    })
+  }
+
   return (
     <nav className='admin-nav'>
         <AdminNavComp link="*/*" name="Hire Requests"/>
         <AdminNavComp link="*/pagecustom/*" name="Website Customization"/>
         <AdminNavComp link="*/manageadmin/*" name="Manage Adminstrators"/>
-        <AdminNavComp link="*" name="Logout"/>
+        <div className='nav-comp'>
+             <span className='nav-comp-logout' onClick={handleLogout} >logout</span>
+         </div>
     </nav>
   )
 }

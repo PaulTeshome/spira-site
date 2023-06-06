f0512475fadf72adf76b4cb9084041a8804b6014
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-07 00:58:27 +0300

    Update reset.js

--- spira/server/routes/reset.js
+++ spira/server/routes/reset.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { passwordReset,passwordResetSubmit } from "../controllers/reset";
+import { passwordReset,passwordResetSubmit } from "../controllers/reset.js";
 
 const router= express.Router();
 

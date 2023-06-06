b94397dc5db7bd0027097bd21389bd3bb2b3474f
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-07 00:57:38 +0300

    Create withAuth.js

---
+++ spira/client/src/components/utils/withAuth.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const withAuth = (Component) => {
+    const AuthenticatedComponent = (props) => {
+    const navigate = useNavigate();
+    const [user,setUser]= useState('')
+    const [alertShown, setAlertShown] = useState(false);
+
+    useEffect(() => {
+      axios.get('/auth/check-login')
+        .then(response => {
+
+            console.log("res:",response)
+            setUser(response.data.user.username)
+            const expirationTime = (response.data.user.exp - response.data.user.iat+1) * 1000;
+            setTimeout(() => {
+              
+              if (!alertShown) {
+                alert('Your session has expired. Please log in again.');
+                setAlertShown(true);
+              }
+            }, expirationTime);
+        })
+        .catch(error => {
+          console.log("error: ",error.response)
+            navigate("/login")
+        });
+    }, [navigate, alertShown]);
+
+
+    return <Component {...props} user={user}/>;
+  };
+
+  return AuthenticatedComponent;
+};
+
+export default withAuth;

4e0f9f4f80147216496b0f5397c8cb5ec087bc48
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-07 18:41:20 +0300

    Update withAuth.js

--- spira/client/src/components/utils/withAuth.js
+++ spira/client/src/components/utils/withAuth.js
@@ -2,11 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-
-
-
-// Define the reducer function
-
 const withAuth = (Component) => {
     const AuthenticatedComponent = (props) => {
     const navigate = useNavigate();

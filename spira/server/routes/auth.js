6da9296d32fd4b61668121ed9f54bc26a335d5c1
Paulos Teshome <88830032+PaulTeshome@users.noreply.github.com>
2023-06-08 00:23:53 +0300

    Update auth.js

--- spira/server/routes/auth.js
+++ spira/server/routes/auth.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { register,login, logout, checkLogin } from "../controllers/auth.js";
+import { login, logout, checkLogin } from "../controllers/auth.js";
 
 const router= express.Router();
 
 router.post('/login', login);
 router.get('/logout', logout);
-router.post('/register', register);
 router.get('/check-login', checkLogin);
 
 export default router;
